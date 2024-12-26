d6a22d9c981105da4ba0df59988f7283cf57bf67
Raj Yadav <94789028+rajyadav282@users.noreply.github.com>
2024-12-26 09:06:11 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -10,7 +10,7 @@ function auth(req, res, next) {
         req.userId = response.id;
         next();
     } else {
-        res.status(403).json({
+        res.status(403).json({  
             message: "Incorrect creds"
         })   
     }   
